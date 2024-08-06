50ea4c696793af6c125cd6b39c05388e094ab1d3
Fzh Khd <fa.khadem18@gmail.com>
2024-08-06 14:08:17 +0430

    make pages

--- app.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-    <p>test</p>
-  </div>
-</template>
---
+++ pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h2>ABout</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, at!
+        Ab vitae sequi consequatur explicabo pariatur quo deleniti reiciendis
+        culpa?
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, at!
+        Ab vitae sequi consequatur explicabo pariatur quo deleniti reiciendis
+        culpa?
+      </p>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped>
+  h2 {
+  
+  margin-bottom: 20px;
+  font-size: 36px;
+  }
+  p{
+      margin: 20px 0;
+  }
+  </style>
+  
