3e8ece13522b47b90f5b5309cc88d7f07b845c7f
Fzh Khd <fa.khadem18@gmail.com>
2024-08-07 08:34:23 +0430

    adding NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt App</NuxtLink>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">about</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/products">products</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
     <h2>home</h2>
     <p>
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, at!
@@ -16,13 +32,12 @@
 
 <script setup></script>
 
-<style  scoped>
+<style scoped>
 h2 {
-
-margin-bottom: 20px;
-font-size: 36px;
+  margin-bottom: 20px;
+  font-size: 36px;
 }
-p{
-    margin: 20px 0;
+p {
+  margin: 20px 0;
 }
 </style>
