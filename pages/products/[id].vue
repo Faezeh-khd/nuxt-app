b26287aab519d7e4610e647aa90689d65d90e14e
Fzh Khd <fa.khadem18@gmail.com>
2024-08-12 11:34:10 +0430

    adding error pages

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -8,7 +8,12 @@
 const { id } = useRoute().params;
 const url = 'https://fakestoreapi.com/products/'+id;
 
+//fetch the product
 const { data: product} = await useFetch(url,{key:id})
+
+if(!product.value){
+  throw createError({statusCode: 404, statusMessage: "Product not found", fatal: true})
+}
 
 definePageMeta({
   layout: "products",
